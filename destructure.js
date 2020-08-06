66b7b16ea82aa13b9108bf18a74d08973dd25b8f
shamiul5vo <shamiulislam15@gmail.com>
2020-08-06 16:08:34 +0600

    done

---
+++ destructure.js
@@ -0,0 +1,5 @@
+const person = { name: "Jack Ryan", age: 37, job: 'actor', gfName: 'Ema', address: 'kochukhet', phone: '017777777777', friends: 'hanks', 'cruise', 'yarn'};
+console.log(person.gfName);
+console.log(person.gfName);
+console.log(person.gfName);
+console.log(person.gfName);
--- inheritance.js
+++ inheritance.js
@@ -1,10 +1,19 @@
-
-class Child{
+class Parent {
+    constructor(){
+        this.fatherName = "Schwerznegger";
+    }
+}
+class Child extends Parent{
     constructor(name){
+        super();
         this.name = name;
+    }
+    getFullName() {
+        return this.name + " " + this.fatherName;
     }
 }
 
-const baby = new Child("Arnond");
+const baby = new Child("Arnold");   
 const baby2 = new Child("Tom");
-console.log(baby);
+console.log(baby.getFullName());
+console.log(baby2.getFullName());
