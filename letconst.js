3149d2baff1e423b370a7c5f99ec1adb3a5a26f5
shamiul5vo <shamiulislam15@gmail.com>
2020-08-06 14:23:02 +0600

    till class

---
+++ letconst.js
@@ -0,0 +1,12 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers [1] = 88;
+numbers.push(12);
+const nayok = {name: "Skib", phone:456789};
+// console.log(numbers) ;
+
+let patientName = "rahim";
+patientName = "fatema" ;
+console.log(patientName);
---
+++ template.js
@@ -0,0 +1,23 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+const fullName = firstName + " " + lastName;
+const fullName2 = `Hello ${firstName} ${20+50+30} is a good man!`;
+
+const multiLine = "I love you\n"
+     + " I miss you\n"
+     + " i need you\n";
+
+ const multiLine2 = `I love you 
+ i miss you 
+ i need you 
+ fuck you`
+
+
+console.log(multiLine2);
+
+
+
+console.log(fullName2);
+
+
+
