f33ac9a6478edaef8097c164f7863db7b5cbe831
shamiul5vo <shamiulislam15@gmail.com>
2020-08-06 16:22:47 +0600

    Done for this module

--- destructure.js
+++ destructure.js
@@ -1,5 +1,10 @@
-const person = { name: "Jack Ryan", age: 37, job: 'actor', gfName: 'Ema', address: 'kochukhet', phone: '017777777777', friends: 'hanks', 'cruise', 'yarn'};
-console.log(person.gfName);
-console.log(person.gfName);
-console.log(person.gfName);
-console.log(person.gfName);
+const person = { name: "Jack Ryan", age: 37, job: 'actor', gfName: 'Ema', address: 'kochukhet', phone: '017777777777', friends: ['hanks', 'cruise', 'yarn']};
+
+const { phone } = person;
+const gfName = person.gfName;
+// const phone = person.phone;
+
+console.log(person.gfName, phone);
+console.log(person.gfName, phone);
+console.log(person.gfName, phone);
+console.log(person.gfName, phone);
